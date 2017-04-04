49cec37605783a9d6266687d52290a4b24e60eb9
Foldblade <foldblade@yeah.net>
2017-04-04 11:43:19 +0800

    Update dailymail-html.py
    2017/4/4更新的html邮件版本

--- dailymail-html.py
+++ dailymail-html.py
@@ -6,12 +6,14 @@ import matplotlib as mpl
 mpl.use('Agg') #在非GUI下能够画图
 import matplotlib.pyplot as plt
 from matplotlib.ticker import  MultipleLocator
+import matplotlib.dates as mdate
 import smtplib  #加载smtplib模块
 from email import encoders
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 import csv
+import pandas
 from bs4 import BeautifulSoup
 
 now = datetime.datetime.now()
@@ -22,7 +24,7 @@ print(nowtime)
 
 url = "http://www.btctrade.com"
 webdata = []
-webdata = urllib.request.urlopen(url).read()
+webdata = urllib.request.urlopen(url,timeout=15).read()
 soup = BeautifulSoup(webdata,"html.parser")
 priceline = str(soup.find(id="rate-btc"))
 #抓取比特币价格网页
@@ -43,68 +45,82 @@ print(btcprice)
 
 with open("data/pricelog-min.csv") as csvfile :
   reader = csv.DictReader(csvfile)
-  x = [row['Time'] for row in reader]
+  date = [row['Time'] for row in reader]
+  csvfile.close()
+x = date
 with open("data/pricelog-min.csv") as csvfile:
   reader = csv.DictReader(csvfile)
-  y = [row['Price'] for row in reader]
-#print(x)
-#print(y)
+  pricelist = [row['Price'] for row in reader]
+  csvfile.close()
+y = pricelist
 
-#xmajorLocator = MultipleLocator(1)
+sum = 0
+for obj in pricelist :
+    sum = sum + float(obj)
+#print(sum)
+average = sum / 1440
+average = round(average,3)
+print('average=',average)
+#求平均值
+
+variance_sum = 0
+for obj in pricelist :
+    variance_min = (float(obj) - average)**2
+    variance_sum = variance_sum + variance_min
+variance = variance_sum / 1440
+variance = round(variance,3)
+print('variance=',variance)
+#求方差
+
+pricemax=max(pricelist)
+pricemin=min(pricelist)
+print(pricemax,pricemin)
+range=float(pricemax)-float(pricemin)
+range=round(range,3)
+print('range=',range)
+#求极差
+
+
 xmajorLocator = MultipleLocator(1)
-plt.figure(dpi=300)
-plt.title('BTC Price Daily'+'\n'+ nowtime, size=14)
-plt.xlabel('Time/min', size=14)
-plt.ylabel('Price/¥', size=14)
-plt.plot(y,linewidth=1.0,)
+fig=plt.figure(figsize=(12,9),dpi=300)
+ax1=fig.add_subplot(111)
+ax1.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M'))#设置时间标签显示格式
+plt.xticks(pandas.date_range(date[0],date[-1],freq='60min'))#时间间隔
+plt.xticks(rotation=90)
+plt.xticks(fontsize = 10)
+ax1.set_xlabel('Time', size=15)
+ax1.set_ylabel('Price/¥', size=15)
+ax1.plot(x,y,linewidth=1.0,label='price')
+ax1.plot(x,[average for obj in x],linewidth=1.0,label='average')
+plt.legend(loc='upper right')
+plt.title('BTC Price Daily'+'\n'+ nowtime, size=25)
 plt.grid(True)
 plt.savefig("data/pricepicture.png",dpi=300)
-print('success!')
+print('drawing successed!')
 
 
-'''
-udn = 0
-f = open('data/pricelog-min-a.txt', 'r+')
-line = f.readline()
-while line:
-    # print(line)
-    a = int(line)
-    udn = udn + int(a)
-    # print(udn)
-    line = f.readline()
-f.close()
-# 读取文件，涨为1，跌为-1。并求“绝对涨落”之和
-# print(udn)
-# “绝对涨落”之和
-audn = udn / 3600
-# print(audn)
-# 求“绝对涨落”平均数
-if audn < 0:
-    suggestion = '以跌为主，不建议入手'
-elif audn == 0:
-    suggestion = '有涨又有落，相对持平'
-elif 0 <= audn < 0.4:
-    suggestion = '涨为主，涨的时间在全天比重不算长'
-elif 0.4 >= audn:
-    suggestion = '总趋势以涨为主，可以考虑卖出'
-#print(suggestion)
-# 给出建议
-'''
 
 message = MIMEMultipart()
 
 # 第三方 SMTP 服务
-mail_host = "smtp.yandex.com"  # SMTP服务器
-mail_user = "email1@email.com"  # 用户名
-mail_pass = "yourpasswd"  # 密码
+mail_host = "smtp.xxxxxx.com"  # SMTP服务器
+mail_user = "xxxx@xxxxxx.com"  # 用户名
+mail_pass = "xxxxxxxxx"  # 密码
 
-sender = 'email1@email.com'  # 发件人邮箱(最好写全, 不然会失败)
-receivers = ['email1@email.com','email2@email.com']  # 接收邮件，可设置多个，如['email1@email.com','email2@email.com']
+sender = 'service1@xxxxxxx.com'  # 发件人邮箱(最好写全, 不然会失败)
+receivers = ['email1@email.com']  # 接收邮件，可设置多个，如['email1@email.com','email2@email.com']
 
-content = '<html>'+'<h1>Html Style</h1>'+ '当前时间:' + nowtime + '<br>' + '比特币价格:' + btcprice  + '<br>' + '技术支持:F.B.'+'</html>'
+content =   '<html>'+'运行时间:' + nowtime + '<br>'          \
+          + '比特币价格:' + btcprice  + '<br>'    \
+          + '24小时极差:'+ str(range) + '<br>'   \
+          + '24小时方差:' + str(variance) + '<br>' \
+          + '24小时平均值:' + str(average)+ '<br>' \
+          + '24小时最大值:' + str(pricemax)+ '<br>'\
+          + '24小时最小值:' + str(pricemin)+ '<br>'\
+          + '技术支持:F.B.'</html>
 #此content请用html写
 title = '比特币价格:' + btcprice  # 邮件主题
-message.attach(MIMEText(content, 'html', 'utf-8'))  # 内容, 格式, 编码
+message.attach(MIMEText(content, 'plain', 'utf-8'))  # 内容, 格式, 编码
 
 message['From'] = "{}".format(sender)
 message['To'] = ",".join(receivers)
