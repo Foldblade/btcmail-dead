963148150bb11a6e9446e1423f52a891e40223b3
Foldblade <foldblade@yeah.net>
2017-04-04 11:31:24 +0800

    Update draw.py
    更新代码，详见readme

--- draw.py
+++ draw.py
@@ -1,13 +1,14 @@
 #encoding:utf-8
+
+#这是邮件附图绘图的代码，dailymail里也有，单独发一遍。
+
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 import datetime
 from matplotlib.ticker import  MultipleLocator
 import matplotlib.dates as mdate
-import matplotlib.ticker as mtick
-import numpy as np
-import pandas as pd
+import pandas
 import csv
 
 now = datetime.datetime.now()
@@ -15,23 +16,41 @@ now.strftime('%Y-%m-%d %H:%M')
 nowtime = now.strftime('%Y-%m-%d %H:%M')
 print(nowtime)
 
+
 with open("data/pricelog-min.csv") as csvfile :
   reader = csv.DictReader(csvfile)
-  x = [row['Time'] for row in reader]
+  date = [row['Time'] for row in reader]
+  csvfile.close()
+x = date
 with open("data/pricelog-min.csv") as csvfile:
   reader = csv.DictReader(csvfile)
-  y = [row['Price'] for row in reader]
-#print(x)
-#print(y)
+  pricelist = [row['Price'] for row in reader]
+  csvfile.close()
+y = pricelist
 
-#xmajorLocator = MultipleLocator(1)
+sum = 0
+for obj in pricelist :
+    sum = sum + float(obj)
+#print(sum)
+average = sum / 1440
+average = round(average,3)
+print('average=',average)
+#求平均值
+
+
 xmajorLocator = MultipleLocator(1)
-plt.figure(dpi=300)
-plt.title('BTC Price Daily'+'\n'+ nowtime, size=14)
-plt.xlabel('Time/min', size=14)
-plt.ylabel('Price/¥', size=14)
-plt.plot(y,linewidth=1.0,)
+fig=plt.figure(figsize=(12,9),dpi=300)
+ax1=fig.add_subplot(111)
+ax1.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M'))#设置时间标签显示格式
+plt.xticks(pandas.date_range(date[0],date[-1],freq='60min'))#时间间隔
+plt.xticks(rotation=90)
+plt.xticks(fontsize = 10)
+ax1.set_xlabel('Time', size=15)
+ax1.set_ylabel('Price/¥', size=15)
+ax1.plot(x,y,linewidth=1.0,label='price')
+ax1.plot(x,[average for obj in x],linewidth=1.0,label='average')
+plt.legend(loc='upper right')
+plt.title('BTC Price Daily'+'\n'+ nowtime, size=25)
 plt.grid(True)
 plt.savefig("data/pricepicture.png",dpi=300)
-print('success!')
-
+print('drawing successed!')
