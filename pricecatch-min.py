eb410a2477281546e0f4f98c75c9136d97912ed6
Foldblade <foldblade@yeah.net>
2017-04-24 23:23:40 +0800

    Update pricecatch-min.py
    A more easy way : API

--- pricecatch-min.py
+++ pricecatch-min.py
@@ -1,6 +1,5 @@
 #encoding:utf-8
 
-from bs4 import BeautifulSoup
 import datetime
 import pandas as pd
 import urllib.request
@@ -9,30 +8,29 @@ import linecache
 now = datetime.datetime.now()
 now.strftime('%Y-%m-%d %H:%M:%S')
 nowtime = now.strftime('%Y-%m-%d %H:%M:%S')
+nowmin = now.strftime('%M')
 print(nowtime)
 
 
-url = "http://www.btctrade.com"
-webdata = []
-webdata = urllib.request.urlopen(url,timeout=10).read()
-soup = BeautifulSoup(webdata,"html.parser")
-priceline = str(soup.find(id="rate-btc"))
+url = "http://api.btctrade.com/api/ticker?coin=btc"
+webdata = str(urllib.request.urlopen(url,timeout=10).read())
 #抓取比特币价格网页
 
-print(priceline)
-a = int(priceline.find('">'))
-#print(btclocation)
-b =int(priceline.find('</i>'))
-#print(ltclocation)
-btcprice=priceline[int(a+2):int(b)]
+print(webdata)
+
+a = int(webdata.find('"last":'))
+#print(lastlocation)
+b =int(webdata.find(',"vol"'))
+#print(vollocation)
+btcprice=webdata[int(a+7):int(b)]
 
 if btcprice[0:1] == '"' :
-  btcprice2=btcprice[1:5]
-  btcprice=btcprice2
-#纠错，初代常会报错，貌似现在不会出现此问题
-
+    btcprice2=btcprice[1:]
+    btcprice=btcprice2
+#纠错,最原始的一个可能bug
 print(btcprice)
 #比特币价格提取
+
 
 btcpriceold = linecache.getline('data/pricelog-min.csv',1441)
 btcpriceold = btcpriceold[int(btcpriceold.find(','))+1:]
@@ -49,3 +47,15 @@ f.write( nowtime + ',' + btcprice)
 f.close()
 #记录价格
 print('record done!')
+
+if nowmin == '00' :
+    data = pd.read_csv('data/pricelog-monh.csv')
+    data = data.drop(0)
+    data.to_csv('data/pricelog-monh.csv', sep=",", index=False)
+    # 删价格首行
+    f = open('data/pricelog-monh.csv', 'a+')
+    f.write(nowtime + ',' + btcprice)
+    f.close()
+    # 记录价格
+#此段为日后升级开发打下基础
+exit()
